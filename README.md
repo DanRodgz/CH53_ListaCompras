47c355b69607d2f9b55d7757742c6e5489b18f1a
Daniel Sebastian Álvarez Rodríguez <daniel.alvarezro96@gmail.com>
2025-04-10 15:59:15 -0600

    Se terminan los últimos puntos del ejercicio

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # CH53_ListaCompras
-Ejercicio de Javascript
+
+**Práctica 100% de Javascript**
+
+---
+
+Se realiza un ejercicio sobre una lista de compras, no se modifica para nada el index.html, ya que todo el ejercicio se basa en la funcionalidad de JavaScript
+
+*La siguiente imagen muestra nuestro HTML*
+
+! [Image] (img/SplashScreen.png)
+
+---
+
+**Los objetivos son los siguientes:**
+ 1. Validar información de los campos
+ 2. Crear un precio por producto al azar
+ 3. Agregar elementos en una tabla
+ 4. Realizar las operaciones para conocer el total de elementos
+ 5. Realizar las operaciones para conocer el total en costo
+ 6. Almacenar la información en el almacenamiento local del navegador
+ 7. Mostrar la información almacenada cuando se abra la página
+ 
+ ---
+
+> Fue creado con plantillas de BootStrap.
--- js/main.js
+++ js/main.js
@@ -15,73 +15,122 @@ let cont = 0;
 let costoTotal = 0;
 let totalEnProductos = 0;
 
-function validarCantidad(){
-    if(txtNumber.value.trim().length<=0){
-        return false;
-    } //length <=0 
+let datos = new Array(); // Almacena los elementos de la tabla [Array]
 
-    if(isNaN(txtNumber.value)){
-        return false;
-    } // isNaN (Not a Number)
+function validarCantidad() {
+  if (txtNumber.value.trim().length <= 0) {
+    return false;
+  } //length <=0
 
-    if(Number(txtNumber.value)>=0){
-        return true;
-    } // >=0 valor es mayor o igual que 0
+  if (isNaN(txtNumber.value)) {
+    return false;
+  } // isNaN (Not a Number)
+
+  if (Number(txtNumber.value) >= 0) {
+    return true;
+  } // >=0 valor es mayor o igual que 0
 } // validarCantidad
 
-    function getPrecio(){
-        return Math.round(Math.random()*10000) / 100;
-    } // getPrecio
+function getPrecio() {
+  return Math.round(Math.random() * 10000) / 100;
+} // getPrecio
 
-btnAgregar.addEventListener("click", function(event){
-    event.preventDefault();
+btnAgregar.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    let isValid = true; // Bandera, al ser true permite agregar los datos a la tabla
+  let isValid = true; // Bandera, al ser true permite agregar los datos a la tabla
 
-    alertValidacionesTexto.innerHTML="";
-    alertValidaciones.style.display="none";
-    txtName.style.border="";
-    txtNumber.style.border="";
+  alertValidacionesTexto.innerHTML = "";
+  alertValidaciones.style.display = "none";
+  txtName.style.border = "";
+  txtNumber.style.border = "";
 
-    txtName.value = txtName.value.trim();
-    txtNumber.value = txtNumber.value.trim();
+  txtName.value = txtName.value.trim();
+  txtNumber.value = txtNumber.value.trim();
 
-    if(txtName.value.length <3 ){
-        txtName.style.border="solid medium red";
-        alertValidacionesTexto.innerHTML="<strong>El nombre del producto no es correcto.</strong>";
-        alertValidaciones.style.display="block";
-        isValid = false;
-    } // length menor que tres, sino se cumple el borde se ilumina de rojo.
+  if (txtName.value.length < 3) {
+    txtName.style.border = "solid medium red";
+    alertValidacionesTexto.innerHTML =
+      "<strong>El nombre del producto no es correcto.</strong>";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+  } // length menor que tres, sino se cumple el borde se ilumina de rojo.
 
-    if(! validarCantidad()){
-        txtNumber.style.border="solid medium red";
-        alertValidacionesTexto.innerHTML+="<br/><strong>La cantidad no es correcta.</strong>";
-        alertValidaciones.style.display="block";
-        isValid = false;
-    } //ValidarCantidad
+  if (!validarCantidad()) {
+    txtNumber.style.border = "solid medium red";
+    alertValidacionesTexto.innerHTML +=
+      "<br/><strong>La cantidad no es correcta.</strong>";
+    alertValidaciones.style.display = "block";
+    isValid = false;
+  } //ValidarCantidad
 
-    if(isValid){ // Si pasó las validaciones
-        cont++; // Primera columna
-        let precio = getPrecio(); // Última columna
-        let row = `<tr>
+  if (isValid) {
+    // Si pasó las validaciones
+    cont++; // Primera columna
+    let precio = getPrecio(); // Última columna
+    let row = `<tr>
                     <td>${cont}</td>
                     <td>${txtName.value}</td>
                     <td>${txtNumber.value}</td>
                     <td>${precio}</td>
-                    </tr>`
-        cuerpoTabla.insertAdjacentHTML("beforeend", row); // Posición que va a tomar en el HTML
-        
-        costoTotal += precio * Number(txtNumber.value);
-        precioTotal.innerText = "$" + costoTotal.toFixed(2);
+                    </tr>`;
 
-        totalEnProductos += Number(txtNumber.value);
-        productosTotal.innerText = totalEnProductos;
+    let elemento = {
+      cont: cont,
+      nombre: txtName.value,
+      cantidad: txtNumber.value,
+      precio: precio,
+    };
+    datos.push(elemento);
+    localStorage.setItem("datos", JSON.stringify(datos)); // Transforma todos los datos a string
 
-        contadorProductos.innerText = cont;
-        
-        txtName.value = "";
-        txtNumber.value = "";
-        txtName.focus();
-    } // if isValid (corrobora que no sean false los otros if)
+    cuerpoTabla.insertAdjacentHTML("beforeend", row); // Posición que va a tomar en el HTML
 
+    costoTotal += precio * Number(txtNumber.value);
+    precioTotal.innerText = "$" + costoTotal.toFixed(2);
+
+    totalEnProductos += Number(txtNumber.value);
+    productosTotal.innerText = totalEnProductos;
+
+    contadorProductos.innerText = cont;
+
+    let resumen = {
+      cont: cont,
+      totalEnProductos: totalEnProductos,
+      costoTotal: costoTotal,
+    };
+    localStorage.setItem("resumen", JSON.stringify(resumen)); // Local storage con JSON
+
+    txtName.value = "";
+    txtNumber.value = "";
+    txtName.focus();
+  } // if isValid (corrobora que no sean false los otros if)
 }); // btnAgregar
+
+window.addEventListener("load", function (event) {
+  event.preventDefault();
+
+  if (this.localStorage.getItem("datos") != null) {
+    datos = JSON.parse(this.localStorage.getItem("datos"));
+  }
+  datos.forEach((d) => {
+    let row = `<tr>
+                <td>${d.cont}</td>
+                <td>${d.nombre}</td>
+                <td>${d.cantidad}</td>
+                <td>${d.precio}</td>
+               </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+  });
+
+  if (this.localStorage.getItem("resumen") != null) {
+    let resumen = JSON.parse(this.localStorage.getItem("resumen"));
+    costoTotal = resumen.costoTotal;
+    totalEnProductos = resumen.totalEnProductos;
+    cont = resumen.cont;
+  } // resumen != null
+
+  precioTotal.innerText = "$" + costoTotal.toFixed(2);
+  productosTotal.innerText = totalEnProductos;
+  contadorProductos.innerText = cont;
+}); // window.addEvenListener load (traer los datos de local storage aún habiendo cerrado la página)
